74fc17fe493fad2a82b76dd16b0e144a1921e22d
Christian Rojas <cjrojas72@gmail.com>
2020-06-02 10:03:26 -0400

    notifications work

--- notification/models.py
+++ notification/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from twitteruser.models import TweetUser
+from django.utils import timezone
 
 
 class Notifications(models.Model):
     user = models.ForeignKey(TweetUser, on_delete=models.CASCADE)
     notification_count = models.IntegerField(default=0)
+    last_checked = models.DateTimeField(default=timezone.now)
--- notification/views.py
+++ notification/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from tweet.models import Tweet
 from notification.models import Notifications
+from django.utils import timezone
 
 
 def notifications(request):
@@ -8,17 +9,21 @@ def notifications(request):
     search_text = ('@{}').format(user)
     at_tweets = Tweet.objects.filter(body__contains=search_text)
     user_notifications = Notifications.objects.get(user=user)
+    user_notifications.last_checked = timezone.now()
     user_notifications.notification_count = 0
     user_notifications.save()
-    print(user_notifications.notification_count)
-    return render(request, 'pages/notifications.html', {'tweets': at_tweets})
+    print('last checked')
+    print(user_notifications.last_checked)
+    return render(request, 'pages/notifications.html', {'tweets': at_tweets, 'notifications': user_notifications.notification_count})
 
 
 def count_notifications(user):
     search_text = ('@{}').format(user)
-    notifications_tweets = Tweet.objects.filter(body__contains=search_text)
-    count = len(notifications_tweets)
     user_notifications = Notifications.objects.get(user=user)
+    checked = user_notifications.last_checked
+    notifications_tweets = Tweet.objects.filter(
+        body__contains=search_text, date__gte=checked)
+    count = len(notifications_tweets)
     user_notifications.notification_count += count
     user_notifications.save()
 
